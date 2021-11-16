a3ccb2d498745286d2d2d0a70b2febc54c09aaee
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-11-16 07:46:18 +0700

    Add files via upload

---
+++ latihan 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	cout<<"++---------------------------++"<<endl;
+	cout<<"||ilmu komputer              ||"<<endl;
+	cout<<"||kelas b                    ||"<<endl;
+	cout<<"||angkatan 2021              ||"<<endl;
+	cout<<"++---------------------------++"<<endl;
+	cout<<"SEMANGAT IKHSAN KAMU PASTI BISA"<<endl;
+	
+	return 0;
+}
+
