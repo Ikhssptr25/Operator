37411a583c67a13fd79bad4067b5f235453cc590
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-12-04 14:44:30 +0700

    Add files via upload

---
+++ pengurangan.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	
+	/*ikhsan saputra
+	  2117051037
+	  kelas B*/
+	  
+   	 // deklarasi variabel//
+	
+	double x; 
+	double y;
+	double z;
+	
+     //input//
+	cin>>x;
+	cin>>y;
+	
+	//MESIN
+	z=x-y;
+	
+	 //output//
+	cout<<endl;
+	cout<<"Hasil pengurangan  :"<< z <<endl;
+	return 0;	
+}
+
