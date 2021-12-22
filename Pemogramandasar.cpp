efc120619a552a05c657a4e9e0e7a591085f6f32
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-12-22 19:44:14 +0700

    Update Pemogramandasar.cpp

--- Pemogramandasar.cpp
+++ Pemogramandasar.cpp
@@ -5,7 +5,7 @@ int main (){
 	cout<<"++---------------------------++"<<endl;
 	cout<<"||Ilmu komputer              ||"<<endl;
 	cout<<"||2117051037                 ||"<<endl;
-	cout<<"||Kelas b                   ||"<<endl;
+	cout<<"||Kelas b                    ||"<<endl;
 	cout<<"||Angkatan 2021              ||"<<endl;
 	cout<<"++---------------------------++"<<endl;
 	cout<<"SEMANGAT IKHSAN KAMU PASTI BISA "<<endl;
